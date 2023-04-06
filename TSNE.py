f0e609ae4bcacc9044d7b947be7db90348f08167
canquanquan <57536602+canquanquan@users.noreply.github.com>
2023-04-05 22:42:20 -0400

    1

--- TSNE.py
+++ TSNE.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
+import tensorflow_probability as tfp
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import seaborn as sb
 
+
 class TSNE:
-    def __init__(self, X, perp, T, eta, alpha, n_components = 2):
+    def __init__(self, X, perp, T, eta, alpha, n_components=2):
         self.X = X
         self.perp = perp
         self.T = T
@@ -15,22 +18,52 @@ class TSNE:
     def fit_transform(self):
         p = self.P()
         Y = 0
-        self.optimize(Y)
+        Y = self.optimize(Y)
+        return Y
 
-    """
-    This function computes the pairwise similarities among datapoints in the high-dimensional dataset X
-    """
-    def P(self):
-        return
+    # This function computes the pairwise similarities among datapoints in the high-dimensional dataset X
+    # Use Gaussian distribution here
+    # return an n*n matrix p
+    # p[i][j] = p_{j|i}
+    def Pi_cond(self, i, var):
+        xi = self.X[i]
+        pi = np.array([np.exp(-np.linalg.norm(xi - xi) ** 2 / (2 * var ** 2)) for xj in self.X])
+        pi[i] = 0
+        return pi / np.sum(pi)
 
-    """
-    This function computes the pairwise similarities among datapoints in the low-dimensional map Y
-    """
+    def P_cond(self, var):
+        num_datapoints = self.X.shape[0]
+        p_cond = []
+        for i in range(num_datapoints):
+            p_cond.append(self.Pi_cond(i, var))
+        return np.array(p_cond)
+
+    # p_{ij} = (p_{j|i} + p_{i|j}) / 2
+    def P(self, var):
+        num_datapoints = self.X.shape[0]
+        p_cond = self.P_cond(var)
+        p = np.zeros((num_datapoints, num_datapoints))
+        for i in range(num_datapoints):
+            for j in range(num_datapoints):
+                p[i][j] = (p_cond[i][j] + p_cond[j][i]) / 2
+        return p
+
+    # This function computes the pairwise similarities among datapoints in the low-dimensional map Y
+    # User Student t-distribution here
+    def Qi(self, i, Y):
+        yi = Y[i]
+        qi = [(1 + np.linalg.norm(yi - yj) ** 2) ** (-1) for yj in Y]
+        qi[i] = 0
+        return qi
+
     def Q(self, Y):
-        return
+        q = [self.Qi(i, Y) for i in range(Y.shape[0])]
+        return q
 
-    def cost_gradient(self, Y):
-        return
+    def cost_gradient(self, p, q, i, Y):
+        j = 0
+        gradient = (p[i][j] - q[i][j]) * (Y[i] - Y[j]) * (1 + np.linalg.norm(Y[i], Y[j]) ** 2) ** (-1)
+        return gradient
 
     def update(self):
         return
@@ -41,7 +74,7 @@ class TSNE:
     def optimize(self, Y):
         t = 0
         is_convergence = False
-        while t<self.T and not is_convergence:
+        while t < self.T and not is_convergence:
             q = self.Q(Y)
             gradient = self.cost_gradient(Y)
             self.update()
@@ -49,3 +82,10 @@ class TSNE:
             if self.check_convergence(): break
 
         return
+
+
+if __name__ == '__main__':
+    X = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
+    test = TSNE(X, perp=3, T=5, eta=10, alpha=10, n_components=2)
+
+    print(test.P(1))
